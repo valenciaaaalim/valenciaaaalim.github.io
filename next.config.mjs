e17ed7c829d5102f5895e87a34093a232e00dae4
valenciaaaalim <110562651+valenciaaaalim@users.noreply.github.com>
2025-10-24 18:16:09 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  output: 'export',                 // <-- add
+  images: { unoptimized: true },    // keep for Pages
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 }
-
 export default nextConfig
